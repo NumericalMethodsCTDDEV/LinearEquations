c67eae994eecaa31795810bab1fea6f277cafbac
Unknown <pohvalister@mail.ru>
2018-03-05 18:05:31 +0300

    some build corrections

--- gtest/gtest_main.cc
+++ gtest/gtest_main.cc
@@ -29,7 +29,8 @@
 
 #include <stdio.h>
 
-#include <gtest/gtest.h>
+//#include <gtest/gtest.h>
+#include "gtest.h"
 
 GTEST_API_ int main(int argc, char **argv) {
   printf("Running main() from gtest_main.cc\n");
