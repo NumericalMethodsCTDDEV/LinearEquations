b4a735db0da63d5bd0847421eaf46549f02120f7
Unknown <pohvalister@mail.ru>
2018-03-06 14:03:18 +0300

    test added

--- testing.cpp
+++ testing.cpp
@@ -60,6 +60,17 @@ TEST(testing, just_test2)
     }
 }
 
+TEST(testing, jacobi_test)
+{
+    matrix_t a = { {3, 1, 2}, {1, -2, 3}};
+    answer_t response = solve(a, "jacobi");
+    EXPECT_TRUE(response.amountOfSolutions == 1);
+    std::vector<double> right_ans = {1, -1};
+    if (response.solution[0] < right_ans[0] - EPS || response.solution[0] > right_ans[0] + EPS)
+        EXPECT_TRUE(false);
+}
+
+
 TEST(testing, seidel_test)
 {
     matrix_t a = { {3, 1, 2}, {1, -2, 3}};
