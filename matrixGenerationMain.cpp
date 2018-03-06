d7c05a9ce1b4553c57cccac2e92ea49171a8e98c
kant97 <kirant9797@gmail.com>
2018-03-06 20:17:01 +0300

    add gilbert matrix generation

--- matrixGenerationMain.cpp
+++ matrixGenerationMain.cpp
@@ -1,36 +1,42 @@
 #include <bits/stdc++.h>
+#include "linearSystemsSolver.h"
+#include <functional>
 
 using namespace std;
 
-const int N = 100;
+const double N = 100;
 
-int main(int argc, char *argv[])
+std::random_device rd;  //Will be used to obtain a seed for the random number engine
+std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+
+void generate(size_t n, std::function<double (int, int, double)> &&genNumber)
 {
-    std::random_device rd;  //Will be used to obtain a seed for the random number engine
-    std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
     std::uniform_real_distribution<> dis(1, N);
-
-    double dom = 0;
-    if (argc == 1) dom = 0;
-    else
-    {
-        dom = 1000;
-    }
-
-    size_t n = 10;
-
-    cout << n << endl;
-
     for (size_t i = 0; i < n; ++i)
     {
         for (size_t j = 0; j < n; ++j)
         {
-            double ok = 0.0;
-            if (i == j) ok = dom;
-            cout << dis(gen) + ok << " ";
+            cout << genNumber(i, j, dis(gen)) << " ";
         }
         cout << endl;
     }
     for (size_t i = 0; i < n; ++i)
         cout << dis(gen) << endl;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3) return 0;
+
+    size_t n = strtoull(argv[1], nullptr, 10);
+    cout << n << endl;
+    string target(argv[2]);
+
+    if (target == "good")
+        generate(n, [](int i, int j, double d) { return i == j ? 1000.0 + d : d; });
+    else if (target == "bad")
+        generate(n, [](int i, int j, double d) {return d;});
+    else if (target == "Gilbert")
+        generate(n, [](int i, int j, double d) {return static_cast<double>(1. / (1 + i + j));});
+    else cout << "undefined matrix" << endl;
 }
